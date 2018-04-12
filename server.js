adcf4b86d711b267fc6d210159cbadb162405cb9
Pablo Varela <pablovarela182@gmail.com>
2018-04-12 20:51:57 +0200

    Add trust file for https

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const app = express()
 
 app.use(express.static(path.join(__dirname, 'dist')))
 
+app.get('/.well-known/acme-challenge/:content', function(req, res) {
+  res.send(
+    'YYFVgXtTlOl9YTMUwscvfUUuzw1v1juRX8c0ckAj_10.kzdtXxNrbjs4N4EqVmaBr5gfW7jaQrZ-2I3dFskj-lY'
+  )
+})
+
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, 'dist/index.html'))
 )
