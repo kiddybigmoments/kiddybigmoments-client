bd08ec6d369aeff858abf023233c73a9d6cd995f
veronica.hellocookyourweb <hello.cookyourweb@gmail.com>
2018-05-02 07:02:17 +0200

    login component modified inserted user component inside

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,16 +1,12 @@
 import { Component, OnInit } from '@angular/core'
-import { NgbTabsetConfig } from '@ng-bootstrap/ng-bootstrap'
+
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.css'],
-  providers: [NgbTabsetConfig] // add NgbTabsetConfig to the component providers
+  styleUrls: ['./login-form.component.css']
 })
 export class LoginFormComponent implements OnInit {
-  constructor(config: NgbTabsetConfig) {
-    // customize default values of tabsets used by this component tree
-    config.justify = 'center'
-    config.type = 'pills'
-  }
+  constructor() {}
+
   ngOnInit() {}
 }
